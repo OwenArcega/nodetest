639968f52af24c2b135a9a137f5bcce1145bc762
Owen Arcega <owenarcega11@gmail.com>
2024-05-28 08:22:45 -0600

    update

--- server.js
+++ server.js
@@ -201,7 +201,7 @@ app.post('/login', (req, res) => {
 
     const { usuario, contrasena } = req.body;
 
-    pool.query(`SELECT id FROM usuarios WHERE nombre = '${usuario}' AND contrasena = '${contrasena}'`,
+    pool.query(`SELECT id FROM usuarios WHERE usuario = '${usuario}' AND contrasena = '${contrasena}'`,
         (error, rows, fields) => {
             if (error) {
                 res.json({
