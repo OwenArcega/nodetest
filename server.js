2c2b35fcbcfb6cec236ede0b9dbe6e16077a3f46
Owen Arcega <owenarcega11@gmail.com>
2024-08-14 12:34:57 -0600

    update

--- server.js
+++ server.js
@@ -14,8 +14,9 @@ const pool = mysql.createPool({
   port: 11456,
 });
 
-app.use(express.json());
 app.use(cors());
+app.use(express.json({ limit: '20mb' }));
+app.use(express.urlencoded({ limit: '20mb', extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Hello world!");
