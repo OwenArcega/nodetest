580ac44d944e673bb2354bcd6129c7b0edda92f5
Owen Arcega <owenarcega11@gmail.com>
2024-08-15 09:23:09 -0600

    update

--- server.js
+++ server.js
@@ -18,6 +18,11 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
+app.use((req,res,next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  next();
+})
+
 app.get("/", (req, res) => {
   res.send("Hello world!");
 });
