e0637d403b63c342ea2547743e890374eba7b230
Owen Arcega <owenarcega11@gmail.com>
2024-08-26 12:38:30 -0600

    update

--- server.js
+++ server.js
@@ -451,13 +451,13 @@ app.post("/mascotaIdeal", (req, res) => {
               const result = await model.generateContent(prompt);
               const response = await result.response;
               const text = response.text();
-              console.log(text);
+              console.log(text)
             }
+            run();
             }
         })
 
 
-run();
 })
 
 app.listen(port, () => {
